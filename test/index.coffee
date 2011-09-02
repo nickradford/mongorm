6856f2518da2c1b2c6a27c617dbb2c957fea4b07
Nick Radford <nick@nicholasradford.com>
2011-09-02 01:45:23 -0400

    moved model test from test/index to test/model

--- test/index.coffee
+++ test/index.coffee
@@ -1,9 +1,2 @@
 mongorm = require '../index'
 
-class PageModel extends mongorm.Model
-    constructor = () ->
-        
-        
-Pages = new PageModel
-
-console.log Pages.getBy 'title', 'New Page'
--- test/index.js
+++ test/index.js
@@ -1,23 +1,4 @@
 (function() {
-  var PageModel, Pages, mongorm;
-  var __hasProp = Object.prototype.hasOwnProperty, __extends = function(child, parent) {
-    for (var key in parent) { if (__hasProp.call(parent, key)) child[key] = parent[key]; }
-    function ctor() { this.constructor = child; }
-    ctor.prototype = parent.prototype;
-    child.prototype = new ctor;
-    child.__super__ = parent.prototype;
-    return child;
-  };
+  var mongorm;
   mongorm = require('../index');
-  PageModel = (function() {
-    var constructor;
-    __extends(PageModel, mongorm.Model);
-    function PageModel() {
-      PageModel.__super__.constructor.apply(this, arguments);
-    }
-    constructor = function() {};
-    return PageModel;
-  })();
-  Pages = new PageModel;
-  console.log(Pages.getBy('title', 'New Page'));
 }).call(this);
